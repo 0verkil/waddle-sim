313907ea61cd967f1919ca467118bdac829a650b
0verkil <112268976+0verkil@users.noreply.github.com>
2025-07-06 19:41:41 -0700

    initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.kt
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import com.necessaryevil.simulatedsdk.Test;
+
+@TeleOp
+class TestOpMode : LinearOpMode() {
+
+    override fun runOpMode() {
+        waitForStart()
+
+        while (opModeIsActive()) {
+            var a = 3.0
+        }
+
+    }
+
+}
