e834113aba413c83d2b5a356c23089e9b0aec087
0verkil <112268976+0verkil@users.noreply.github.com>
2025-08-11 09:07:22 -0700

    feat: arm

--- src/test/kotlin/com/necessaryevil/waddle/MechanismDemo.kt
+++ src/test/kotlin/com/necessaryevil/waddle/MechanismDemo.kt
@@ -6,11 +6,13 @@ import com.necessaryevil.waddle.ftc.Simulation
 import com.necessaryevil.waddle.ftc.hardware.SimulatedDcMotor
 import com.necessaryevil.waddle.ftc.time.SimulatableElapsedTime
 import com.necessaryevil.waddle.physics.common.SimulatedMotor
+import com.necessaryevil.waddle.physics.mechanism.Arm
 import com.necessaryevil.waddle.physics.mechanism.LinearExtension
 import com.qualcomm.robotcore.hardware.DcMotor
 import io.kotest.core.spec.style.FunSpec
 import org.psilynx.psikit.Logger
 import org.psilynx.psikit.wpi.Pose2d
+import org.psilynx.psikit.wpi.Rotation2d
 
 /**
  * Example PID class.
@@ -91,11 +93,14 @@ class MechanismSimulator : FunSpec( {
         val rightSlide = SimulatedHardware.addMotor("rightSlide", SimulatedMotor.GOBILDA_435, SimulatedDcMotor.MotorType.GOBILDA_435)
 
         // add a linear extension with spool radius 25mm
-        SimulatedHardware.addSimulationObject(LinearExtension("Slides", -0.15, 1.0, 11.0, 40.0, 45.0, 25.0, leftSlide, rightSlide))
+        //SimulatedHardware.addSimulationObject(LinearExtension("Slides", -0.15, 1.0, 11.0, 40.0, 0.0, 25.0, leftSlide, rightSlide))
+        SimulatedHardware.addSimulationObject(Arm("Arm", -0.15, 1.0, 11.0, 0.0, 180.0, leftSlide, rightSlide, gearRatio = 10.0))
 
         Simulation.addSimulation(MechanismDemo(), 120.0)
 
-        Simulation.runNextSimulation { Logger.recordOutput("Simulation/Pose", Pose2d()); Logger.recordOutput("Simulation/Left", leftSlide.state.get(2)) }
+        Simulation.runNextSimulation { Logger.recordOutput("Simulation/Pose", Pose2d(72.0 * 0.0254, 72.0 * 0.0254,
+            Rotation2d()
+        )); Logger.recordOutput("Simulation/Left", leftSlide.state.get(2)) }
 
     }
 
